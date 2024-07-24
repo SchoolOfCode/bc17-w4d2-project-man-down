fbdd14ed4e88c05191fe348e71f878f0aa17f87e
Sambambamm <165159759+Coder-samm@users.noreply.github.com>
2024-07-24 12:12:36 +0100

    Added middleware Helmet

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
-const express = require ('express')
+import express from 'express'
 const app = express()
 const port = 3000
 
+import helmet from 'helmet'
+app.use(helmet())
+
 app.get('/',(req, res)=> {
-    res.sendStatus('Hello World')
+    res.send('Hello World')
 })
+
 app.listen(port, () => {
     console.log(`app listening on port ${port}`)
   })
